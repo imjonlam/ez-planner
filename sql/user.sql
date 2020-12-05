caf12cac912630c6c7d59bc8d794a0db13fc18e0
teabubblez <jonathanlam.1994@gmail.com>
2020-12-05 15:39:08 -0500

    changed password length

--- sql/user.sql
+++ sql/user.sql
@@ -3,7 +3,7 @@ CREATE TABLE IF NOT EXISTS users (
     email VARCHAR(50) PRIMARY KEY,
     first_name VARCHAR(25) NOT NULL,
     last_name VARCHAR(25) NOT NULL,
-    password VARCHAR(50) NOT NULL,
+    password VARCHAR(69) NOT NULL,
     address VARCHAR(30) NOT NULL,
     city VARCHAR(30) NOT NULL,
     province VARCHAR(30) NOT NULL,
