cec51e9f33c1800106ac5ed03c5ae3ec0fba3c07
auchimaru <aidan.uchimaru@gmail.com>
2020-12-05 14:44:09 -0500

    beginning of login process

---
+++ panel/home/home.php
@@ -0,0 +1,55 @@
+<?php
+            $servername = "localhost";
+            $username = "auchimar";
+            $password = "yinhayd<";
+            $dbname = "auchimar";
+
+            $conn = mysqli_connect($servername, $username, $password, $dbname);
+
+            if(isset($_POST['login'])) {
+                echo "Found";
+            }
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+    <script src="https://code.jquery.com/jquery-3.5.1.js"
+        integrity="sha256-QWo7LDvxbWT2tbbQ97B53yJnYU3WhH/C8ycbRAkjPDc=" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css"
+        integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link
+        href="https://fonts.googleapis.com/css2?family=Prompt:ital,wght@0,100;0,300;0,700;0,800;1,100;1,600;1,700&display=swap"
+        rel="stylesheet">
+    <link rel="stylesheet" href="../../css/cps530.css">
+    <script>
+        $(function () {
+          $("#nav-placeholder").load("../../common/navbar.html");
+          $("#footer").load("../../common/footer.html");
+        });
+      </script>
+</head>
+
+<body>
+    <header id="nav-placeholder"></header>
+    <div class="container">
+        
+    </div>
+
+
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
+        integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj"
+        crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"
+        integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN"
+        crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js"
+        integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s"
+        crossorigin="anonymous"></script>
+</body>
+
+</html>
