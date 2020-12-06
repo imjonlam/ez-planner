b908502708bab403b3b66c3877c8c286d4895967
teabubblez <jonathanlam.1994@gmail.com>
2020-12-06 15:27:01 -0500

    testing

--- php/groups/createGroup.php
+++ php/groups/createGroup.php
@@ -8,15 +8,7 @@
 
     $response = array();
     if ($stmt->execute()) {
-        $stmt->close();
-
-        $sql = "INSERT INTO group_members (group_name, user) VALUES (?, ?)";
-        $stmt = $conn->prepare($sql);
-        $stmt->bind_param("ss", $_POST["group"],  $_POST["email"]);
-        
-        if ($stmt->execute()) {
-            $response["success"] = "success";
-        }        
+        $response["success"] = "success";
     } else {
         $response["error"] = "fail"
     }
