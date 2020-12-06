8e08d6b3f10cf2bfc1dd93f186592b278bfa1d27
teabubblez <jonathanlam.1994@gmail.com>
2020-12-06 17:46:27 -0500

    fixed loading data into cards

--- php/groups/getFlight.php
+++ php/groups/getFlight.php
@@ -2,7 +2,7 @@
     include '../connect.php';
     $conn = connect();
 
-    $sql = "SELECT airline, class, origin, origin_code, destination, destination_code, price, stops, duration, img FROM flights, group_flights WHERE group_name = ? AND airline = flight";
+    $sql = "SELECT airline, class, origin, origin_code, destination, destination_code, price, stops, duration, img FROM flights, group_flights WHERE group_name = ? AND flight = flight_id";
     $stmt = $conn->prepare($sql);
     $stmt->bind_param("s", $_GET["group"]);
     $stmt->execute();
